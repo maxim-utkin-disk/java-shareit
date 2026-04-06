34bd2128b347888defc6c5c7337d8dce5623c16b
Maxim Utkin <maxim-utkin-disk@yandex.ru>
2026-04-07 00:04:08 +0300

    Спринт 14 - перезаливка из неправильного репозитория

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,26 @@
 package ru.practicum.shareit.item.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.FieldDefaults;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode(of = {"id"})
 public class Item {
+    Long id;
+    @NotBlank(message = "Наименование предмета бронирования не может быть пустым")
+    String name;
+    @NotBlank(message = "Описание предмета бронирования не должно быть пустым")
+    String description;
+    @NotNull(message = "Нужно указать статус предмета бронирования: он занят или свободен")
+    Boolean available;
+    Long owner;
+    Long request;
 }
